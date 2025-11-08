55f127c8e3c6bc81568e8aba61033b9619edeea0
David Agardh <agardh.david@gmail.com>
2025-11-08 12:00:09 +0100

    Remove distrobox patch
    No longer needed because of upstream fix

--- build_files/build.sh
+++ build_files/build.sh
@@ -39,11 +39,5 @@ cp /ctx/cosign.pub /etc/pki/davidagardh-cosign.pub
 rsync -r /ctx/root_files/ /
 rm /etc/profile.d/nano-default-editor.sh
 
-# Fix distrobox-init fish problem
-# Only until https://github.com/89luca89/distrobox/pull/1672 is merged
-dnf5 install -y patch
-patch -p1 /usr/bin/distrobox-init /ctx/distrobox-autoload-fish_prompt.patch
-dnf5 remove -y patch
-
 /ctx/lint-fixes.sh
 
